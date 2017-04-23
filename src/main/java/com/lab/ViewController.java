7f9f40ba2175116d261ff4f6f24afccd0e7026e6
Hany Abdelrahman <medhat.hani@gmail.com>
2017-04-23 19:27:54 +0200

    Include Metrics

--- src/main/java/com/lab/ViewController.java
+++ src/main/java/com/lab/ViewController.java
@@ -1,13 +1,9 @@
 package com.lab;
 
-import java.text.MessageFormat;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.jcs.JCS;
-import org.apache.commons.jcs.access.CacheAccess;
-import org.apache.commons.jcs.engine.behavior.ICacheElement;
-import org.hibernate.cache.CacheException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,43 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
 import com.lab.models.View;
 import com.lab.repositories.ViewRepository;
 
 @RestController
 public class ViewController {
-    private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
     
-    private CacheAccess<Long, View> cache = null;
+    private static final String GET_VIEWS_COUNT_METRIC = "get-views-metric";
+    
+    private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
+    private MetricRegistry metrics;
     
     @Autowired
     ViewRepository viewRepository;
     
-    @RequestMapping("/view")
-    Iterable<View> getTeams() {
-        return viewRepository.findAll();
-    }
-    
-    @RequestMapping("/view/{videoId}/{userId}")
-    View handleView(@PathVariable long id) throws InterruptedException {
-        logger.info(MessageFormat.format("Fetching team with id {0}", id));
-        View item = null;
-        ICacheElement<Long, View> pair = cache.getCacheElement(id);
-        if (pair == null) {
-            logger.info(MessageFormat.format("Item with id {0} not found in cache. Fetching from database", id));
-            item = viewRepository.findOne(id);
-            if (item != null) {
-                cache.put(id, item);
-            }
-            return item;
-        } else {
-            item = pair.getVal();
-        }
-        return item;
-    }
-    
     @RequestMapping("count/{videoId}")
     public String getViewCount(@PathVariable long videoId) {
+        Meter meter = metrics.meter(GET_VIEWS_COUNT_METRIC);
+        meter.mark();
         return "" + viewRepository.countByVideoId(videoId);
     }
     
@@ -65,10 +46,13 @@ public class ViewController {
     
     @PostConstruct
     public void init() {
-        try {
-            cache = JCS.getInstance("default");
-        } catch (CacheException e) {
-            e.printStackTrace();
-        }
+        metrics = new MetricRegistry();
+        
+        final Slf4jReporter reporter = Slf4jReporter.forRegistry(metrics)
+                .outputTo(LoggerFactory.getLogger(ViewController.class))
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(1, TimeUnit.SECONDS);
     }
 }
